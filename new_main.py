73f2e749d1680b6e8688174e02b8470394b4c894
MaxMavaIll <illvauser8@gmail.com>
2023-02-19 09:51:17 +0200

    v0.1.0

---
+++ new_main.py
@@ -0,0 +1,17 @@
+import json
+import funtion as f
+import logging as log
+import time
+from environs import Env
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+
+def main():
+    pass
+
+
+
+
+if __name__ == "__main__":
+    main()
