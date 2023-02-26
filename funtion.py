3a3a6c558fdbd5503911c8e581dfc6c9564e4a0b
MaxMavaIll <illvauser8@gmail.com>
2023-02-26 21:29:33 +0200

    Good version v2.0

--- funtion.py
+++ funtion.py
@@ -8,16 +8,14 @@ from datetime import timedelta, datetime
 
 
 path_file_out=abspath("out.json")
-get_data_txhash = {}
+# get_data_txhash = {}
 
-def terminal(cmd: str = None, password: str = None):
+def terminal(cmd: str = None, password: str = "None"):
     try:
-        print("start")
         cmd = cmd.split()
         p1 = subprocess.Popen(["echo", password], stdout=subprocess.PIPE)
         p2 = subprocess.Popen(cmd, stdout=subprocess.PIPE, stdin=p1.stdout)
         output, err = p2.communicate()
-        print("erro1", err)
         p1.stdout.close()
         p2.stdout.close()
         return output.decode('utf-8')
@@ -45,18 +43,26 @@ def write_file(network: str, id: str):
     with open(path_file_out, "w") as file:
         json.dump(data, file)
 
-def get_vote_id_and_last_time(dict: dict, vote_last_time: bool):
-    id = [] 
-    for proposol in dict["proposals"]:
+def get_vote_id_and_last_time(str_terminal: str, config: dict, vote_last_time: bool, network: str):
+    get_votes = json.loads(terminal(str_terminal, config["pass"]))
 
+    validator_add = config["addr"]
+
+    with open(path_file_out, "r") as file:
+        data = json.load(file)
+
+
+    id = [] 
+    for proposol in get_votes["proposals"]:
+        
         if proposol["status"] == "PROPOSAL_STATUS_VOTING_PERIOD":
-            if vote_last_time:
-                if check_last_time_vote(proposol["voting_end_time"]):
-                    try:
-                        id.append(proposol["proposal_id"])
-                    except:
-                        id.append(proposol["id"])
-            else:
+                
+
+            a = proposol["proposal_id"] not in data[network]
+            b = check_last_time_vote(proposol["voting_end_time"], proposol["voting_start_time"], vote_last_time)
+            c = no_vote_validator("{} q gov vote {} {} {} {} -o json".format(config["bin"], proposol["proposal_id"], validator_add, config["node"], config["chain_id"]), config, proposol["proposal_id"], network)
+
+            if a and b and c:
                 try:
                     id.append(proposol["proposal_id"])
                 except:
@@ -64,108 +70,222 @@ def get_vote_id_and_last_time(dict: dict, vote_last_time: bool):
 
     return id
 
+def get_json_file(path: str, mode: str):
+    with open(path, mode) as file:
+        return file
+
+def no_vote_validator(str_terminal: str, config: dict, id: str | int, network: str):
+    try:
+        votes = json.loads(terminal(str_terminal))#.format(config["bin"], config["from"], config["keyring"]))
+        votes = votes["options"]["option"].title().replace("_", "")
+        logging.info("Validator: {} | {} has already voted {} from {}".format(config["from"], config["addr"], votes, id))
+        with open("out.json", "r") as file:
+            date = json.load(file)
+        
+        date[network].append(id)
+        with open("out.json", "w") as file:
+            json.dump(date, file)
+        
+        return False
+    
+    except:
+        logging.info("Validator: {} | {} hasn`t voted {} from {} yet")
+        return True
+
+# def in_five_hour_start(time_isoparse: str, vote_last_time: bool):
+    
+#     if not vote_last_time:
+#         date = parser.isoparse(time_isoparse).replace(tzinfo=None)
+#         now = datetime.utcnow()
+
+#         if now - date >= 5:
+#             return True
+        
+#         return False
+#     else:
+
+#         return False
 
 def check_config():
     config = get_config()
-    mass = ["", "--fees ", "--node ", "--chain-id ", "--from ", "--keyring-backend ", "", "", ""]
-    save = dict["vote_last_moment"]
-    del dict["vote_last_moment"]
-    for network, configs in dict.items():
+    save = config["vote_last_moment"]
+    del config["vote_last_moment"]
+
+    
+
+    for network in config.keys():
         i = 0
-        
-        for key in configs:
-            if configs[key] != "":
-                dict[network][key] = mass[i] + configs[key]
-            i+=1
 
-        dict[network]["explorer"] = dict[network]["explorer"].replace(" ", "").split(",")
-        if dict[network]["vote"].lower() in ['none', 'yes', 'no'] :
-            dict[network]["vote"] = dict[network]["vote"].lower()
-
-        else:
-            data = dict[network]["vote"].lower().replace(" ", "").split(",")
-            for index in range(len(data)):
-                data[index] = data[index].split("-")
+        if config[network]["fees"] != "":
+            config[network]["fees"] = "--fees " + config[network]["fees"]
+        if config[network]["node"] != "":
+            config[network]["node"] = "--node " + config[network]["node"]
+        if config[network]["chain_id"] != "":
+            config[network]["chain_id"] = "--chain-id " + config[network]["chain_id"]
+        if config[network]["from"] != "":
+            config[network]["from"] = "--from " + config[network]["from"]
+        if config[network]["keyring"] != "":
+            config[network]["keyring"] = "--keyring-backend " + config[network]["keyring"]
         
-            dict[network]["vote"] = data
+
+        config[network]["explorer"] = config[network]["explorer"].replace(" ", "").split(",")
+        
+        config[network]["addr"] = terminal("{} keys show {} -a {} ".format(config[network]["bin"], config[network]["from"].replace("--from ", ""), config[network]["keyring"]), config[network]["pass"])
+    
+
+        check_existing_file(network)
+        
+        # config[network]["vote"] = get_variant_with_more_votes()
+
+        # vote_split = config[network]["vote"].split(",")
+        # if len(config[network]["vote"].split(",")) == 1:
+        #     if "".sw
+
+        # if config[network]["vote"].lower() in ['none', 'yes', 'no'] :
+        #     config[network]["vote"] = config[network]["vote"].lower()
+
+        # else:
+        #     data = config[network]["vote"].lower().replace(" ", "").split(",")
+        #     for index in range(len(data)):
+        #         data[index] = data[index].split("-")
+        
+        #     config[network]["vote"] = data
     # dict["vote_last_moment"] = save
-    return save
+    return config, save
 
-def check_existing_file(path_from_main_dir: str = ""):
-    path_file = abspath(path_from_main_dir)
-    if not exists(path_file):
-        with open(path_file, "w") as file:
-            json.dump({}, file)
-   
-    return True
+
+
+def check_existing_file(network: str):
+    # path_file = abspath(path_file_out)
+    if not exists(path_file_out):
+        mass = {}
+        mass[network] = []
+    else:
+        with open(path_file_out, "r") as file:
+            mass = json.load(file)
+
+        if network not in mass:
+            mass[network] = []
+    
+
+    with open(path_file_out, "w") as file:
+        json.dump(mass, file)
     
 
 
 
 def check_voted(network: str, id: str, configs: list | str):
-    vote = answer = 'yes'
-    if check_existing_file("out.json") and configs != "none":
-        with open(path_file_out, "r") as file:
-            data = json.load(file)
-        # print(type(configs), type(list))
-        if type(configs) == type(list()):
-            for index, vol in enumerate(configs):
-                if int(configs[index][0]) == id and len(configs[index]) == 2:
-                    vote = configs[index][1]
-                elif int(configs[index][0]) == id and len(configs[index]) == 3:
-                    answer = False
-                    vote = configs[index][1]
-                    return answer, vote
-        elif type(configs) == type(str()):
-            vote = configs
-        #     if vote[index][0] == id and vote[index][1] == 'none':
-        #         return True
 
-        if network in data:
-            for out_id in data[network]:
-                if out_id == int(id) :
-                    logging.info(f"I voted for this proposol {id}")
-                    answer = True
-        else:
-            answer = False
+   
+    with open(path_file_out, "r") as file:
+        data = json.load(file)
+    # print(type(configs), type(list))
+    if type(configs) == type(list()):
+        for index, vol in enumerate(configs):
+            if int(configs[index][0]) == id and len(configs[index]) == 2:
+                vote = configs[index][1]
+            elif int(configs[index][0]) == id and len(configs[index]) == 3:
+                answer = False
+                vote = configs[index][1]
+                return answer, vote
+    elif type(configs) == type(str()):
+        vote = configs
+    #     if vote[index][0] == id and vote[index][1] == 'none':
+    #         return True
 
-
-        return answer, vote
+    if network in data:
+        for out_id in data[network]:
+            if out_id == int(id) :
+                logging.info(f"I voted for this proposol {id}")
+                answer = True
     else:
-        return answer, None
+        answer = False
 
+
+    return answer, vote
+    
 def save_vote(network: str, id: str):
     if exists(path_file_out):
         pass
 
 
-def form_request(text: bytes, network: str, id: str, url_explorer_tx: str,
-                url_explorer_proposol: str, name:str):
+def vote_for_proposal(str_terminal: str, config: dict, id: str):
     
+
     try:
-        data = json.loads(text)
-        get_data_txhash[id] = {"txhash": url_explorer_tx + data["txhash"],
-                                "name": name, 
-                                "proposol": url_explorer_proposol + id}
+        output = terminal(str_terminal, config["pass"])
+        data = json.loads(output)
 
         write_file(network, id)
-        logging.info(f"Proposle {id} {data}")
+        
+        return {"txhash": config['explorer'][0] + data["txhash"],
+                                "name": config["from"].replace("--from ", ""), 
+                                "proposol": config['explorer'][1] + id}
     except Exception as error:
-        logging.error("error\n", error)
+        name = config["from"]
+        addr = config["addr"]
+        logging.info(f"Proposol {id} not vote\nValidator: {name} | {addr}")
+        return {"txhash": f"Proposol {id} not vote\nValidator: {name} | {addr}", 
+                "name": config["from"].replace("--from ", ""),
+                "proposol": config['explorer'][1] + id}
 
-def send_to_server():
-    global get_data_txhash
+def send_to_server(get_data_txhash: dict):
+    
     logging.info(f"{get_data_txhash != {}}")
     if get_data_txhash != {}:
-        usr_server(get_data_txhash)
-        get_data_txhash = {}
+        try:
+            usr_server(get_data_txhash)
+            get_data_txhash = {}
+        except Exception as error:
+            logging.info(f"Error server:\n{error}")
+            logging.info(f"I save get_data_txhash\n{get_data_txhash}")
 
-def check_last_time_vote(time_isoparse: dict):
-    date = parser.isoparse(time_isoparse).replace(tzinfo=None)
-    now = datetime.utcnow()
-    date = date - now
+def check_last_time_vote(time_isoparse_finish: str, time_isoparse_start: str, vote_last_time: bool):
 
-    if date <= timedelta(hours=2):
-        return True
-    
-    return False
+    if vote_last_time:
+        date = parser.isoparse(time_isoparse_finish).replace(tzinfo=None)
+        now = datetime.utcnow()
+        date = date - now
+
+        if date <= timedelta(hours=2):
+            return True
+        
+        return False
+    else:
+        date = parser.isoparse(time_isoparse_start).replace(tzinfo=None)
+        now = datetime.utcnow()
+        date = now - date
+
+        if date >= timedelta(hours=5):
+            return True
+        
+        return False
+
+
+def get_num_vote(votes: json):
+
+    mass = {}
+
+    for num, value in enumerate( votes["votes"]):
+        gov = value["options"][0]["option"]
+        print(f"{num}. {gov}")
+
+        t_v_vote = gov.split("_", 2)[-1]
+        if t_v_vote not in mass.keys():
+            mass[t_v_vote] = 0
+
+        mass[t_v_vote] += 1
+
+    return mass
+
+def get_variant_with_more_votes(srt_terminal: str) -> str:
+    votes = json.loads(terminal(srt_terminal))
+
+    num_variant = get_num_vote(votes)
+
+    max_votes = max(list(num_variant.values()))
+
+    for key in num_variant.keys():
+        if num_variant[key] == max_votes:
+            return key.title().replace("_", "")
+
--- main.py
+++ main.py
@@ -1,41 +1,27 @@
 import json
 import funtion as f
 import logging as log
-import time
-from environs import Env
+# from apscheduler.schedulers.background import BackgroundScheduler
 
-logger = log.getLogger(__name__)
+get_data_txhash = {}
 
 def main():
-    log.basicConfig(
-        level=log.DEBUG,
-        format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
-    )
-    
-    config = f.get_config()
-    vote_last_time = f.check_config(config)
-    
-    
-    # f.check_config(config)
-    log.debug(config)
-    for network, configs in config.items():
-        log.info(f"ne1twork {network}")
-        to_terminal = "{} q gov proposals {} --reverse --limit 20 -o json ".format(configs["bin"], configs["node"])
-        get_votes = json.loads(f.terminal(to_terminal, config[network]["pass"]))
-        
-        for index, id in enumerate( f.get_vote_id_and_last_time(get_votes, vote_last_time)):
-            answer, vote = f.check_voted(network, id, configs["vote"])
-            if not answer:
-                to_terminal = "{} tx gov vote {} {} {} {} {} {} {} -o json -y".format(configs["bin"], id, vote, configs["fees"], configs["node"], 
-                                                                        configs["chain_id"], configs["keyring"], configs["from"])
-                log.info(f"Command {to_terminal}")
-                f.form_request(f.terminal(to_terminal, config[network]["pass"]), network, id, config[network]['explorer'][0],
-                        config[network]['explorer'][1], config[network]['from'].replace("--from ", ""))
-                time.sleep(10)
+    config, vote_last_moment = f.check_config()
 
-            else:
-                log.info(f"I skip Network {network}. And this proposol {id}")
-        f.send_to_server()
+    for network, configs in config.items():
+        id_vote_proposols = f.get_vote_id_and_last_time("{} q gov proposals {} --reverse --limit 20 -o json ".format(configs["bin"], configs["node"]), configs, vote_last_moment, network)
+
+        for id in id_vote_proposols:
+            position = f.get_variant_with_more_votes("{} q gov votes {} {} {} -o json".format(configs["bin"], id, configs["chain_id"], configs["node"]))
+
+            get_data_txhash[id] = f.vote_for_proposal("{} tx gov vote {} {} {} {} {} {} {} -o json -y".format(configs["bin"], id,
+                                                                                        position, configs["fees"], 
+                                                                                        configs["node"], configs["chain_id"],
+                                                                                        configs["keyring"], configs["from"]),
+                                                                                        configs, network, id )
+
+        f.send_to_server(get_data_txhash)
+
             
 if __name__ == "__main__":
     main()
