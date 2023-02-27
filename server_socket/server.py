d2bb63f527ff237966cb416e869960cf75433d27
MaxMavaIll <illvauser8@gmail.com>
2023-02-27 13:57:23 +0100

    edit server

--- server_socket/server.py
+++ server_socket/server.py
@@ -1,7 +1,7 @@
 import socketserver
 import sys
-import json, logging
-
+import json
+import logging as log
 
 
 path_data_file = "data/pack_for_send.json"
@@ -14,15 +14,25 @@ class EchoHandler(socketserver.BaseRequestHandler):
     def handle(self):
         with open(path_data_file, "r") as file:
             data = json.load(file)
-        data_with_message = self.request.recv(1024).strip()
-        logging.info(data, data_with_message)
-        data[self.client_address[0]] = json.loads(data_with_message.decode("utf-8"))
-        logging.info(data, self.client_address[0])
-        logging.info("Address: {}".format(self.client_address[0]))
-        logging.info("Data: {}".format(data_with_message.decode("utf-8")))
+
+        data_with_message = b""
+        while True:
+            chunk = self.request.recv(1024)
+            data_with_message += chunk
+            if len(chunk) < 1024:
+                break
+        data_with_message = data_with_message.decode()
+        data[self.client_address[0]] = json.loads(data_with_message)
+        log.info("Address: {}".format(self.client_address[0]))
+        log.info("Data: {}".format(data_with_message))
         write_dict(data)
         # socket.sendto()
     
 if __name__ == "__main__":
+    log.basicConfig(
+        level=log.INFO,
+        format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
+    )
+
     with socketserver.TCPServer(('', 2000), EchoHandler) as server:
         server.serve_forever()
