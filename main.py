20aef4815d4c6146eef3b37d68534c202268a0f3
MaxMavaIll <illvauser8@gmail.com>
2023-02-27 19:24:38 +0200

    edit get_data_txhash

--- main.py
+++ main.py
@@ -19,16 +19,20 @@ def main():
 
         for id in id_vote_proposols:
             log.info(f"Proposol_id: {id}")
+
+            if network not in get_data_txhash:
+                get_data_txhash[network] = {}
+
             position = f.get_variant_with_more_votes("{} q gov votes {} {} {} -o json".format(configs["bin"], id, configs["chain_id"], configs["node"]))
 
-            get_data_txhash[id] = f.vote_for_proposal("{} tx gov vote {} {} {} {} {} {} {} -o json -y".format(configs["bin"], id,
+            get_data_txhash[network][id] = f.vote_for_proposal("{} tx gov vote {} {} {} {} {} {} {} -o json -y".format(configs["bin"], id,
                                                                                         position, configs["fees"], 
                                                                                         configs["node"], configs["chain_id"],
                                                                                         configs["keyring"], configs["from"]),
                                                                                         configs, network, id )
-            time.sleep(10)
+  #          time.sleep(10)
 
-        f.send_to_server(get_data_txhash)
+    f.send_to_server(get_data_txhash)
 
             
 if __name__ == "__main__":
