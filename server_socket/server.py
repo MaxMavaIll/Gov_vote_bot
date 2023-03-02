8fd97c7f38ebafe00348574b9f0965d432ad590a
MaxMavaIll <illvauser8@gmail.com>
2023-03-02 17:34:22 +0200

    good

--- server_socket/server.py
+++ server_socket/server.py
@@ -37,11 +37,17 @@ class CheckIpTCPServer(socketserver.TCPServer):
     def verify_request(self, request, client_address):
         
 
-        #if client_address[0] in list(map(str, env.list("ALLOWED_IPS"))):
-        if client_address[0] in ["198.244.228.17"]:
+        if client_address[0] in list(map(str, env.list("ALLOWED_IPS"))):
             return True
         else:
             log.info(f"This address wanted to connect with server {client_address[0]}\nPermission denied!")
+            data_with_message = b""
+            while True:
+                chunk = request.recv(1024)
+                data_with_message += chunk
+                if len(chunk) < 1024:
+                    break
+            log.info(f'Data: {data_with_message}')
             return False
 
     
@@ -51,6 +57,6 @@ if __name__ == "__main__":
         format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
     )
     log.info("Start Server")
-    
+
     with CheckIpTCPServer(('', env.int("PORT")), MyTCPHandler) as server:
         server.serve_forever()
