b89d2569f88e2296aa45cab73b3a69ffff243a95
MaxMavaIll <illvauser8@gmail.com>
2023-03-31 11:12:05 +0300

    Update

--- scheduler/job.py
+++ scheduler/job.py
@@ -28,18 +28,20 @@ async def add_user_checker(bot: Bot):
     for ip in list(data):
         for network in data[ip]:
             for id in data[ip][network]:
-                if data[ip][network][id]["txhash"] != '':
+                
                     txhash = data[ip][network][id]["txhash"]
-                    name_wallet = data[ip][network][id]["name"]
-                    await bot.send_message(chat_id=config.tg_bot.chat_id, text=f"{name_wallet} {network} {id}\n"
-                                                                           f"{txhash}")
-                else:
                     error = data[ip][network][id]["error"]
                     name_wallet = data[ip][network][id]["name"]
 
-                    for admin_id in config.tg_bot.admin_ids:
-                        await bot.send_message(chat_id=admin_id, text=f"{name_wallet} {network} {id}\n"
+                    if txhash != '':
+                        await bot.send_message(chat_id=config.tg_bot.chat_id, text=f"{name_wallet} {network} {id}\n"
+                                                                           f"{txhash}")
+
+                    if error != '':
+                        for admin_id in config.tg_bot.admin_ids:
+                            await bot.send_message(chat_id=admin_id, text=f"{name_wallet} {network} {id}\n"
                                                                            f"{error}")
+                            
         del data[ip]
     logging.info(f"Data_end: {data}")   
 
